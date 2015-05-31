cf4c9b1ffaa33ed944a54e3b71f658ec4ee96a61
Kristoffer Dalby <kradalby@kradalby.no>
2015-05-31 22:43:41 +0200

    Last time I said it works? I was kidding.  Try this.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 os=$(lsb_release -d -s)
 uptime=$(uptime)
 
-curl -i \
+curl -i --silent \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
 -X POST --data '{"internal":"'"$ip"'","hostname":"'"$hostname"'","os":"'"$os"'","uptime":"'"$uptime"'"}' \
